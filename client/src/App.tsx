d6ee434330639a5e75b51423a4734aa93707ed08
Ivan Escobar <ivnescbr1989@gmail.com>
2023-07-08 08:29:05 -0400

    vite react setup

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <header>Berry Fine Jewelry</header>
+      <main></main>
+      <footer>All rights reserved</footer>
+    </div>
+  )
+}
+
+export default App

149b3e413ed1c367113cbc1f2827fe401608df37
Ivan Escobar <ivnescbr1989@gmail.com>
2023-07-09 10:20:05 -0400

    style home

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,15 +1,15 @@
 import logo from "../assets/berry_logo01.png";
 export default function NavBar() {
   return(
-  <div>
+  <div id="nav-bar">
     <img id="logo" src={logo} alt="Berry Fine Jewelry logo"></img>
-    <section>
-        <a href="">Home</a>
-        <a href="">Shop</a>
-        <a href="">About</a>
-        <a href="">Contact</a>
-        <a href="">Book Online</a>
-        <a href="">Log in/Sign up</a>
+    <section id="nav-links">
+        <a href="">Home </a>
+        <a href="">Shop </a>
+        <a href="">About </a>
+        <a href="">Contact </a>
+        <a href="">Book Online </a>
+        <a href="">Log in/Sign up </a>
     </section>
   </div>)
 }
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,3 +1,9 @@
+import logoB from '../assets/berry_logo_mark.png'
 export default function HomePage() {
-    return <></>
+    return (
+    <div>
+        <h1>Home</h1>
+        <hr></hr>
+        <img src={logoB} alt="berry fine jewelry logo" id='berry-logo'/>
+    </div>)
 }

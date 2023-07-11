2bc76e6766385bedfe76121e276c9718e2003c1f
Ivan Escobar <ivnescbr1989@gmail.com>
2023-07-10 21:10:33 -0400

    service item

---
+++ client/src/components/ServiceItem.tsx
@@ -0,0 +1,13 @@
+import berrylogo from '../assets/berry_logo_mark.png'
+export default function ServiceItem() {
+    return (
+        <div id="service-card">
+            <img src={berrylogo} alt="berry logo"  id='sl-img'/>
+  <section id='sl-text' >  
+<h2>Service Item</h2>
+<p>this is a short description of the service offered. kakoii desu</p>
+<p>Price:  $44.94</p></section>
+
+        </div>
+    )
+}

05d9874d11b2c59f077e94e35db4bcfa64cb4c07
Ivan Escobar <ivnescbr1989@gmail.com>
2023-07-17 20:28:10 -0400

    servie items iterated in component

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,11 +1,9 @@
 import logoB from '../assets/berry_logo_mark.png'
-import ServiceItem from '../components/ServiceItem'
 export default function HomePage() {
     return (
     <div>
         <h1>Home</h1>
         <hr></hr>
-        <ServiceItem/>
         <img src={logoB} alt="berry fine jewelry logo" id='berry-logo'/>
     </div>)
 }
--- client/src/pages/Services.tsx
+++ client/src/pages/Services.tsx
@@ -1,9 +1,11 @@
 import logoB from '../assets/berry_logo_mark.png'
+import ServiceItem from '../components/ServiceItem'
 export default function Services() {
     return (
     <div>
-        <h1>Book On line</h1>
-        <hr></hr>
+        <h2>Book On line</h2>
+        <hr/>
+        <ServiceItem/>
         <img src={logoB} alt="berry fine jewelry logo" id='berry-logo'/>
     </div>)
 }
